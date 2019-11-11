23bf8950b2af26caa7f15da159d3b76962ff02d9
Mark Hazleton <mhazleton@cecentertainment.com>
2019-11-10 19:47:48 -0600

    clean up and simplify

---
+++ Mwh.SampleCRUD/Controllers/BaseApiController.cs
@@ -0,0 +1,27 @@
+﻿using Mwh.SampleCRUD.BL.Repositories;
+using System;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Mwh.SampleCrud.Controllers
+{
+    public abstract class BaseApiController : ApiController
+    {
+        protected BaseApiController() { }
+
+        public IEmployeeDB empDB
+        {
+            get
+            {
+                if (HttpContext.Current.Application == null)
+                    return new EmployeeMock();
+
+                if ((EmployeeMock)HttpContext.Current.Application["EmployeeDB"] == null)
+                    HttpContext.Current.Application["EmployeeDB"] = new EmployeeMock();
+
+                return ((EmployeeMock)HttpContext.Current.Application["EmployeeDB"]);
+            }
+        }
+    }
+}
