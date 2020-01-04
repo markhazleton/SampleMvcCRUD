89d7e71231d89a845eaeea1a484a6c7fc7b4dd8b
Mark Hazleton <mhazleton@cecentertainment.com>
2020-01-04 11:27:31 -0600

    Add Application Insights

--- Mwh.Sample.WebApi/App_Start/FilterConfig.cs
+++ Mwh.Sample.WebApi/App_Start/FilterConfig.cs
@@ -6,7 +6,7 @@ namespace Mwh.Sample.WebApi
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
