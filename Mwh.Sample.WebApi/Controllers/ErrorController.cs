a460d996374e17f1a93fb62452c05a714a4c982f
Mark Hazleton <mhazleton@cecentertainment.com>
2020-04-03 17:19:28 -0500

    Add XML Comments and HttpGet to Error Pages

--- Mwh.Sample.WebApi/Controllers/ErrorController.cs
+++ Mwh.Sample.WebApi/Controllers/ErrorController.cs
@@ -7,11 +7,20 @@ namespace Mwh.Sample.WebApi.Controllers
     /// </summary>
     public class ErrorController : Controller
     {
-        // GET: Error
+        /// <summary>
+        /// General Error Page for Most Errors
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+        /// <summary>
+        /// Error Page for File Not Found (404)
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
         public ActionResult NotFound()
         {
             return View();
