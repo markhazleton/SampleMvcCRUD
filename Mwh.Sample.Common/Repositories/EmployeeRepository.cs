761f1649ab0ce8fb975b64783fde731aae2ebc15
Mark Hazleton <mhazleton@cecentertainment.com>
2020-04-04 15:18:45 -0500

    Add Repository and Services

---
+++ Mwh.Sample.Common/Repositories/EmployeeRepository.cs
@@ -0,0 +1,36 @@
+﻿using Mwh.Sample.Common.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mwh.Sample.Common.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly EmployeeMock _emp;
+
+        public EmployeeRepository() { _emp = new EmployeeMock(); }
+
+        public async Task AddAsync(EmployeeModel employee, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            await Task.Run(() => _emp.Update(employee)).ConfigureAwait(true);
+        }
+
+        public Task<EmployeeModel> FindByIdAsync(int id, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            return Task.Run(() => _emp.Employee(id));
+        }
+
+        public Task<IEnumerable<EmployeeModel>> ListAsync(CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            return Task.Run(() => (IEnumerable<EmployeeModel>)_emp.EmployeeCollection());
+        }
+
+        public void Remove(EmployeeModel employee) { _emp.Delete(employee.EmployeeID); }
+
+        public void Update(EmployeeModel employee) { _emp.Update(employee); }
+    }
+}
---
+++ Mwh.Sample.Common/Repositories/EmployeeResponse.cs
@@ -0,0 +1,23 @@
+﻿using Mwh.Sample.Common.Models;
+
+namespace Mwh.Sample.Common.Repositories
+{
+    public class EmployeeResponse : BaseResponse<EmployeeModel>
+    {
+        /// <summary>
+        /// Creates a success response.
+        /// </summary>
+        /// <param name="employee">Saved employee.</param>
+        /// <returns>Response.</returns>
+        public EmployeeResponse(EmployeeModel employee) : base(employee)
+        { }
+
+        /// <summary>
+        /// Creates am error response.
+        /// </summary>
+        /// <param name="message">Error message.</param>
+        /// <returns>Response.</returns>
+        public EmployeeResponse(string message) : base(message)
+        { }
+    }
+}
