2de2450f1cd1c27fe0b2b218d8dea121a0c62b7b
Mark Hazleton <mark.hazleton@gmail.com>
2021-11-03 10:37:36 -0500

    Create Project for IHttpClientFactory as alternative to REST Sharp implementation

---
+++ Mwh.Sample.Common.HttpClientFactory/Clients/EmployeeRestClient.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Http;
+using Mwh.Sample.Common.Interfaces;
+using Mwh.Sample.Common.Models;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mwh.Sample.Common.HttpClientFactory.Clients
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class EmployeeRestClient : RestClientBase, IEmployeeClient
+    {
+        public EmployeeRestClient(IHttpContextAccessor httpContextAccessor, IHttpClientFactory httpClientFactory) : base($"{httpContextAccessor.HttpContext.Request.Scheme}://{httpContextAccessor.HttpContext.Request.Host}{httpContextAccessor.HttpContext.Request.PathBase}", "employee", httpClientFactory)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmployeeRestClient"/> class.
+        /// </summary>
+        /// <param name="apiPath">The API path.</param>
+        /// <param name="appName">Name of the application.</param>
+        public EmployeeRestClient(string apiPath, string appName, IHttpClientFactory httpClientFactory) : base(apiPath, appName, httpClientFactory)
+        {
+        }
+
+        /// <summary>
+        /// delete as an asynchronous operation.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="token">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>EmployeeResponse.</returns>
+        public async Task<EmployeeResponse> DeleteAsync(int id, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            var resp = await Delete<EmployeeResponse>($"/api/employee/{id}").ConfigureAwait(true);
+            return resp == null ? new EmployeeResponse("Null Returned from REST Call to Delete") : resp;
+        }
+
+        /// <summary>
+        /// find by identifier as an asynchronous operation.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="token">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>EmployeeModel.</returns>
+        public async Task<EmployeeModel> FindByIdAsync(int id, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            return await GetAsync<EmployeeModel>($"/api/employee/{id}").ConfigureAwait(true);
+        }
+
+        /// <summary>
+        /// list as an asynchronous operation.
+        /// </summary>
+        /// <param name="token">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>IEnumerable&lt;EmployeeModel&gt;.</returns>
+        public async Task<IEnumerable<EmployeeModel>> GetAsync(CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            return await GetAsync<List<EmployeeModel>>($"/api/employee").ConfigureAwait(true);
+        }
+
+        /// <summary>
+        /// save as an asynchronous operation.
+        /// </summary>
+        /// <param name="employee">The employee.</param>
+        /// <param name="token">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>EmployeeResponse.</returns>
+        public async Task<EmployeeResponse> SaveAsync(EmployeeModel employee, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            var resp = await Post<EmployeeResponse>($"/api/employee", employee).ConfigureAwait(true);
+            return resp;
+        }
+
+        /// <summary>
+        /// update as an asynchronous operation.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="employee">The employee.</param>
+        /// <param name="token">The cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>EmployeeResponse.</returns>
+        public async Task<EmployeeResponse> UpdateAsync(int id, EmployeeModel employee, CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+
+            if (employee.id != id)
+                return new EmployeeResponse($"Mismatch in id({id}) && id({employee.id}).");
+
+            var resp = await Put<EmployeeResponse>($"/api/employee/{id}", employee).ConfigureAwait(true);
+            return resp;
+        }
+    }
+}
---
+++ Mwh.Sample.Common.HttpClientFactory/Clients/RestClientBase.cs
@@ -0,0 +1,238 @@
+﻿using Mwh.Sample.Common.Clients;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Mwh.Sample.Common.HttpClientFactory.Clients
+{
+    /// <summary>
+    /// Class ClientBase.
+    /// Implements the <see cref="System.IDisposable" />
+    /// </summary>
+    /// <seealso cref="System.IDisposable" />
+    public abstract class RestClientBase : IDisposable, IRestClientBase
+    {
+
+        /// <summary>
+        /// Lazy Client used to instantiate when needed rather than during constructor
+        /// </summary>
+        private IHttpClientFactory _lazyClient;
+
+        /// <summary>
+        /// ClientBase constructor used to set Application Name and Base Url for requests
+        /// </summary>
+        /// <param name="baseUrl">The base URL.</param>
+        /// <param name="appName">Name of the application.</param>
+        protected RestClientBase(string baseUrl, string appName, IHttpClientFactory clientFactory)
+        {
+            if (string.IsNullOrEmpty(appName))
+                baseUrl = "MISSING";
+
+            AppName = appName;
+            BaseAPIUrl = baseUrl.Trim('/');
+            _lazyClient = clientFactory;
+        }
+
+        /// <summary>
+        /// Client Base Destructor part of the IDisposable implementation
+        /// </summary>
+        ~RestClientBase() { Dispose(false); }
+
+        /// <summary>
+        /// Gets the HttpClient for this class,a lazy pattern is used to create an instance when needed but never more
+        /// than a single instance
+        /// </summary>
+        /// <returns>RestClient.</returns>
+        /// <exception cref="ObjectDisposedException">RestClient has been disposed</exception>
+        protected HttpClient Client()
+        {
+            if (_lazyClient == null)
+                throw new ObjectDisposedException("RestClient has been disposed");
+            return _lazyClient.CreateClient();
+        }
+
+        /// <summary>
+        /// Deletes the specified URL segment.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="urlSegment">The URL segment.</param>
+        /// <returns>T.</returns>
+        protected async Task<T> Delete<T>(string urlSegment)
+        {
+            HttpRequestMessage request = GetRequestMessage(urlSegment, HttpMethod.Delete);
+            using var response = await Client().SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            if (response.IsSuccessStatusCode)
+            {
+                // perhaps check some headers before deserializing
+                try
+                {
+                    return await response.Content.ReadFromJsonAsync<T>();
+                }
+                catch (NotSupportedException) // When content type is not valid
+                {
+                    IsError = true;
+                    Console.WriteLine("The content type is not supported.");
+                }
+                catch (JsonException) // Invalid JSON
+                {
+                    IsError = true;
+                    Console.WriteLine("Invalid JSON.");
+                }
+            }
+            return default;
+        }
+
+        /// <summary>
+        /// Client Base Dispose Property part of the IDisposable implementation
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            // There are no unmanaged resources to release, but
+            // if we add them, they need to be released here.
+        }
+
+        /// <summary>
+        /// Execute HttpGet and return results
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="urlSegment">The URL segment.</param>
+        /// <returns>T.</returns>
+        protected async Task<T> GetAsync<T>(string urlSegment)
+        {
+            HttpRequestMessage request = GetRequestMessage(urlSegment, HttpMethod.Get);
+            using var response = await Client().SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            if (response.IsSuccessStatusCode)
+            {
+                // perhaps check some headers before deserializing
+                try
+                {
+                    return await response.Content.ReadFromJsonAsync<T>();
+                }
+                catch (NotSupportedException) // When content type is not valid
+                {
+                    IsError = true;
+                    Console.WriteLine("The content type is not supported.");
+                }
+                catch (JsonException) // Invalid JSON
+                {
+                    IsError = true;
+                    Console.WriteLine("Invalid JSON.");
+                }
+            }
+            return default;
+        }
+
+        private HttpRequestMessage GetRequestMessage(string urlSegment, HttpMethod method)
+        {
+            var request = new HttpRequestMessage(method, new Uri($"{BaseAPIUrl}{urlSegment}"));
+            request.Headers.TryAddWithoutValidation("UserID", UserID.ToString());
+            request.Headers.TryAddWithoutValidation("Application", AppName);
+            request.Headers.TryAddWithoutValidation("MachineName", Environment.MachineName);
+            return request;
+        }
+
+        /// <summary>
+        /// Posts the specified URL segment.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="urlSegment">The URL segment.</param>
+        /// <param name="requestBody">The request body.</param>
+        /// <returns>T.</returns>
+        protected async Task<T> Post<T>(string urlSegment, object requestBody)
+        {
+            HttpRequestMessage request = GetRequestMessage(urlSegment, HttpMethod.Post);
+            request.Content = JsonContent.Create(requestBody);
+            using var response = await Client().SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            if (response.IsSuccessStatusCode)
+            {
+                // perhaps check some headers before deserializing
+                try
+                {
+                    return await response.Content.ReadFromJsonAsync<T>();
+                }
+                catch (NotSupportedException) // When content type is not valid
+                {
+                    IsError = true;
+                    Console.WriteLine("The content type is not supported.");
+                }
+                catch (JsonException) // Invalid JSON
+                {
+                    IsError = true;
+                    Console.WriteLine("Invalid JSON.");
+                }
+            }
+            return default;
+
+        }
+
+        /// <summary>
+        /// Puts the specified URL segment.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="urlSegment">The URL segment.</param>
+        /// <param name="requestBody">The request body.</param>
+        /// <returns>T.</returns>
+        protected async Task<T> Put<T>(string urlSegment, object requestBody)
+        {
+            HttpRequestMessage request = GetRequestMessage(urlSegment, HttpMethod.Put);
+            request.Content = JsonContent.Create(requestBody);
+            using var response = await Client().SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            if (response.IsSuccessStatusCode)
+            {
+                // perhaps check some headers before deserializing
+                try
+                {
+                    return await response.Content.ReadFromJsonAsync<T>();
+                }
+                catch (NotSupportedException) // When content type is not valid
+                {
+                    IsError = true;
+                    Console.WriteLine("The content type is not supported.");
+                }
+                catch (JsonException) // Invalid JSON
+                {
+                    IsError = true;
+                    Console.WriteLine("Invalid JSON.");
+                }
+            }
+            return default;
+        }
+
+        /// <summary>
+        /// Client Base Dispose Property part of the IDisposable implementation
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(false);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// The Name of the Client Application making the API Call
+        /// </summary>
+        public string AppName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The Root of the Domain with the API
+        /// </summary>
+        public string BaseAPIUrl { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Public Flag for when the API Encounters and Error
+        /// </summary>
+        public bool IsError { get; set; }
+
+        /// <summary>
+        /// The Status of the last request made from the API Client
+        /// </summary>
+        public string Status { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The User ID that is used to make the request, this defaults to Web Account
+        /// </summary>
+        public int UserID { get; set; }
+    }
+}
