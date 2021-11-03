4875f3fb4b454b8b4b663fee3db542b271a61e22
Mark Hazleton <mark.hazleton@gmail.com>
2021-11-03 10:38:21 -0500

    Add Interface for Rest Clients, remove REST Sharp dependent classes

---
+++ Mwh.Sample.Common/Interfaces/IRestClientBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Mwh.Sample.Common.Clients
+{
+    public interface IRestClientBase
+    {
+        void Dispose();
+
+        string AppName { get; set; }
+        string BaseAPIUrl { get; set; }
+        bool IsError { get; set; }
+        string Status { get; set; }
+    }
+}
