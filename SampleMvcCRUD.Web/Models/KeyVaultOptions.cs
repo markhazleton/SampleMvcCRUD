c6e8c8964774d9635746f4f24f3e7896c45948d3
Mark Hazleton <mark.hazleton@gmail.com>
2022-01-29 12:01:25 -0600

    update npm package

--- SampleMvcCRUD.Web/Models/KeyVaultOptions.cs
+++
@@ -1,9 +0,0 @@
-
-namespace SampleMvcCRUD.Web.Models;
-public class KeyVaultOptions
-{
-    public KeyVaultUsage Mode { get; set; }
-    public string KeyVaultUri { get; set; }
-    public string ClientId { get; set; }
-    public string ClientSecret { get; set; }
-}
