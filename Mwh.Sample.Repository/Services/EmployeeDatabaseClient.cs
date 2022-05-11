f49d2840d662fe556f9d2d9e05a4f5dc42b78e4a
Mark Hazleton <MAH0190@BSWHealth.org>
2022-05-11 10:37:34 -0500

    Add database client so that docker image does not need network to call Api

---
+++ Mwh.Sample.Repository/Services/EmployeeDatabaseClient.cs
@@ -0,0 +1,63 @@
+﻿
+namespace Mwh.Sample.Repository.Services
+{
+    public class EmployeeDatabaseClient : IDisposable, IEmployeeClient
+    {
+        private readonly IEmployeeService service;
+
+        public EmployeeDatabaseClient(IEmployeeService service)
+        {
+            this.service = service;
+        }
+
+        public async Task<int> AddMultipleEmployeesAsync(string?[]? namelist)
+        {
+            return await service.AddMultipleEmployeesAsync(namelist);
+        }
+
+        public async Task<EmployeeResponse> DeleteAsync(int id, CancellationToken token)
+        {
+            return await service.DeleteAsync(id, token).ConfigureAwait(false);
+        }
+
+        public void Dispose()
+        {
+            ((IDisposable)service).Dispose();
+        }
+
+        public async Task<DepartmentDto> FindDepartmentByIdAsync(int id, CancellationToken token)
+        {
+            return await FindDepartmentByIdAsync(id, token).ConfigureAwait(false);  
+        }
+
+        public async Task<EmployeeResponse> FindEmployeeByIdAsync(int id, CancellationToken token)
+        {
+            return await service.FindEmployeeByIdAsync(id, token).ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<DepartmentDto>> GetDepartmentsAsync(CancellationToken token)
+        {
+            return await service.GetDepartmentsAsync(token).ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<EmployeeDto>> GetEmployeesAsync(CancellationToken token)
+        {
+            return await service.GetEmployeesAsync(token).ConfigureAwait(false);
+        }
+
+        public async Task<EmployeeResponse> SaveAsync(EmployeeDto employee, CancellationToken token)
+        {
+            return await service.SaveAsync(employee, token).ConfigureAwait(false);
+        }
+
+        public async Task<DepartmentResponse> SaveAsync(DepartmentDto dept, CancellationToken token)
+        {
+            return await service.SaveAsync(dept, token).ConfigureAwait(false);
+        }
+
+        public async Task<EmployeeResponse> UpdateAsync(int id, EmployeeDto? employee, CancellationToken token)
+        {
+            return await service.UpdateAsync(id, employee, token).ConfigureAwait(false);
+        }
+    }
+}
