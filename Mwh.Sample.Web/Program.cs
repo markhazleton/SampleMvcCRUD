7207224c21777446033e81572769721e373dc59a
Mark Hazleton <mark.hazleton@controlorigins.com>
2022-08-23 08:29:50 -0500

    Add workflow to publish to Azure Linux App service

--- Mwh.Sample.Web/Program.cs
+++ Mwh.Sample.Web/Program.cs
@@ -24,6 +24,7 @@ builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddSession();
 builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
 builder.Services.AddControllersWithViews();
+builder.Services.AddApplicationInsightsTelemetry(builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
 
 var app = builder.Build();
 app.UseMyHttpContext();
