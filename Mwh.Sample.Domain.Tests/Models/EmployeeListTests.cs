0e17ff625fbb9e58662bec9ad4a8a9ae7df4faa2
Mark Hazleton <mark.hazleton@controlorigins.com>
2022-09-22 18:23:54 -0500

    add employee list with tests

---
+++ Mwh.Sample.Domain.Tests/Models/EmployeeListTests.cs
@@ -0,0 +1,53 @@
+﻿namespace Mwh.Sample.Domain.Tests.Models;
+
+[TestClass]
+public class EmployeeListTests
+{
+    [TestMethod]
+    public void AddItem_Null_ExpectedBehavior()
+    {
+        // Arrange
+        var employeeList = new EmployeeList();
+        EmployeeDto? item = null;
+
+        // Act
+        var result = employeeList.AddItem(item);
+
+        // Assert
+        Assert.AreEqual(result.EnumerateItems().Count(), 0);
+    }
+
+    [TestMethod]
+    public void AddItem_Valid_ExpectedBehavior()
+    {
+        // Arrange
+        var employeeList = new EmployeeList();
+        EmployeeDto? item = new(
+            3,
+            "Test",
+            33,
+            "TX",
+            "USA",
+            EmployeeDepartmentEnum.HR
+            );
+
+        // Act
+        var result = employeeList.AddItem(item);
+
+        // Assert
+        Assert.AreEqual(result.EnumerateItems().Count(), 1);
+    }
+
+    [TestMethod]
+    public void EnumerateItems_Initialization_ExpectedBehavior()
+    {
+        // Arrange
+        var employeeList = new EmployeeList();
+
+        // Act
+        var result = employeeList.EnumerateItems();
+
+        // Assert
+        Assert.AreEqual(result.Count(), 0);
+    }
+}
---
+++ Mwh.Sample.Domain/Models/EmployeeList.cs
@@ -0,0 +1,24 @@
+﻿namespace Mwh.Sample.Domain.Models;
+
+/// <summary>
+/// Employee List
+/// </summary>
+public class EmployeeList
+{
+    private readonly List<EmployeeDto> _list = new();
+
+    public EmployeeList AddItem(EmployeeDto? item)
+    {
+        if (item == null) return this;
+        _list.Add(item);
+        return this;
+    }
+    public List<EmployeeDto> List => _list;
+
+
+    public IEnumerable<EmployeeDto> EnumerateItems()
+    {
+        foreach (var item in _list)
+            yield return item;
+    }
+}
